b0234a141a9d2984f3acef17b151bc668df35fec
Babs Ofe <babsofe9@gmail.com>
2023-10-25 09:50:00 +0200

    updated

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,40 +1,13 @@
 import { useEffect, useRef } from 'react'
 import LogoS from '../../../assets/images/logo-s.png'
 import './index.scss'
-import { gsap } from "gsap-trial";
-import { CustomEase } from "gsap-trial/CustomEase";
 
 const Logo = () => {
   const bgRef = useRef()
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  useEffect(() => {
-    gsap.registerPlugin(CustomEase)
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
-  }, [])
+  
 
   return (
     <div className="logo-container" ref={bgRef}>
